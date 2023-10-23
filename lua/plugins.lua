f50481133bd115cba175e0c989784882bd98a48e
root309 <choco00101x@gmail.com>
2023-10-24 02:45:24 +0900

    add markdown-preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -325,5 +325,10 @@ return function()
         }
         use 'folke/tokyonight.nvim'
         use 'norcalli/nvim-colorizer.lua'
+        use({
+            "iamcco/markdown-preview.nvim", -- :MarkdownPreview
+            run = function() vim.fn["mkdp#util#install"]() end,
+        })
+        use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
     end)
 end
