fe1e7fdec12832fb7d8dd51a7c2291d1d192afd6
root309 <choco00101x@gmail.com>
2024-01-01 01:32:12 +0900

    refactor plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,124 +1,94 @@
 return {
-    {
-        'MunifTanjim/nui.nvim'
-    },
-    --{'andweeb/presence.nvim'},
-    {'hoob3rt/lualine.nvim'},
-    {'hrsh7th/nvim-compe'},
-    {'junegunn/fzf'},
-    {'junegunn/fzf.vim'},
-    {'tpope/vim-fugitive'},
-    {'norcalli/nvim-colorizer.lua'},
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    },
-    --theme
-    {
-        'gbprod/nord.nvim'
-    },
-    {
-      "craftzdog/solarized-osaka.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-    },
-    {'npxbr/gruvbox.nvim'},
+    -- UI Enhancements
+    { 'MunifTanjim/nui.nvim' },
+    { 'hoob3rt/lualine.nvim' },
+    { 'xiyaowong/transparent.nvim' },
+    { 'akinsho/bufferline.nvim' },
+    { 'goolord/alpha-nvim' },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+
+    -- Fuzzy Finder and File Explorer
+    { 'junegunn/fzf' },
+    { 'junegunn/fzf.vim' },
+    { 'nvim-tree/nvim-tree.lua' },
+    { 'nvim-tree/nvim-web-devicons' },
+
+    -- Git Integration
+    { 'tpope/vim-fugitive' },
+
+    -- Color and Theme
+    { 'norcalli/nvim-colorizer.lua' },
+    { 'gbprod/nord.nvim' },
+    { "craftzdog/solarized-osaka.nvim", priority = 1000, opts = {} },
+    { 'npxbr/gruvbox.nvim' },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {'folke/tokyonight.nvim'},
-    {'rebelot/kanagawa.nvim'},
-    -- install without yarn or npm
+    { 'folke/tokyonight.nvim' },
+    { 'rebelot/kanagawa.nvim' },
+
+    -- Markdown Preview
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
         build = function() vim.fn["mkdp#util#install"]() end,
     },
-    -- install with yarn or npm
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         build = "cd app && yarn install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
+        init = function() vim.g.mkdp_filetypes = { "markdown" } end,
         ft = { "markdown" },
     },
-    {'xiyaowong/transparent.nvim'},
-    -- neo tree
-    --{'nvim-neo-tree/neo-tree.nvim'},
-    {'nvim-tree/nvim-tree.lua'},
-    {'nvim-lua/plenary.nvim'},
-    {'nvim-tree/nvim-web-devicons'},
-    {'MunifTanjim/nui.nvim'},
-    -- lsp
+
+    -- LSP and Autocomplete
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { 'neovim/nvim-lspconfig' },
+    { 'onsails/lspkind-nvim' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'hrsh7th/nvim-compe' },
+
+    -- Treesitter and Related Plugins
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+    { 'windwp/nvim-ts-autotag' },
+    { 'windwp/nvim-autopairs' },
+
+    -- Telescope
     {
-      "williamboman/mason.nvim", -- LSP Installer
-      dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-        "nvim-lua/plenary.nvim",
-      }
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        requires = { {'nvim-lua/plenary.nvim'} }
     },
-    {'onsails/lspkind-nvim'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/nvim-cmp'},
+
+    -- Rust Analyzer
     {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
+        'neovim/nvim-lspconfig',
+        config = function() require('lspconfig').rust_analyzer.setup{} end
     },
-    -- tag snippet
-    {'windwp/nvim-ts-autotag'},
-    {'windwp/nvim-autopairs'},
-    -- bufferline
-    {'akinsho/bufferline.nvim'},
-    -- alpha-nvim
-    {'goolord/alpha-nvim'},
-    -- indent
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-    -- rust-analyzer
-    {
-      'neovim/nvim-lspconfig',
-      config = function()
-      require('lspconfig').rust_analyzer.setup{}
-      end
-    },
-    -- noice.nvim
+
+    -- Noice UI for Neovim
     {
         "folke/noice.nvim",
         event = "VeryLazy",
-        opts = {
-        -- add any options here
-        },
+        opts = {},
         dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
         }
     },
-    -- nvim-notify
-    { 
+
+    -- Notifications
+    {
         'rcarriga/nvim-notify',
-        opts = {
-			    timeout = 5000,
-		    },
+        opts = { timeout = 5000 },
     },
-    -- copilot
-    {
-      "github/copilot.vim",
-      lazy = false,
-    }, 
-    -- lightline
-    {
-      "itchyny/lightline.vim",
-      lazy = false,
-    },
-    {
-      'mengelbrecht/lightline-bufferline',
-      lazy = false,
-    }
+
+    -- Other Utilities
+    { "github/copilot.vim", lazy = false },
+    { "itchyny/lightline.vim", lazy = false },
+    { 'mengelbrecht/lightline-bufferline', lazy = false },
 }
+
