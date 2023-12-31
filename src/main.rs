099b16d94a649960d3a85931c6301617ed598c9a
root309 <choco00101x@gmail.com>
2024-01-01 02:52:07 +0900

    Fix Extracting only the commit hash portion

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use std::process::Command;
+use std::str;
 
 fn main() {
-    let local_commit = Command::new("git")
+    let local_commit_output = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
-        .expect("Failed to execute git command")
-        .stdout;
+        .expect("Failed to execute git command");
 
-    let remote_commit = Command::new("git")
+    let local_commit = str::from_utf8(&local_commit_output.stdout)
+        .expect("Invalid UTF-8")
+        .trim();
+
+    let remote_commit_output = Command::new("git")
         .args(&["ls-remote", "https://github.com/root309/nvim.git", "HEAD"])
         .output()
-        .expect("Failed to execute git command")
-        .stdout;
+        .expect("Failed to execute git command");
+
+    let remote_commit = str::from_utf8(&remote_commit_output.stdout)
+        .expect("Invalid UTF-8")
+        .trim()
+        .split_whitespace()
+        .next()
+        .expect("No remote commit found");
 
     if local_commit == remote_commit {
         println!("Your Neovim configuration is up to date.");
