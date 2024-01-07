5785bb9375e3ef9a30ffe6331b352b98282d1da5
root309 <choco00101x@gmail.com>
2024-01-08 05:36:22 +0900

    delete old config directory

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,33 +1,42 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-# Function to create a symbolic link
+
 function Create-SymbolicLink {
     param (
         [string]$LinkPath,
         [string]$TargetPath
     )
     
-    # Backup existing files or directories, if any
     if (Test-Path $LinkPath) {
         $backupPath = "$LinkPath.backup"
-        Write-Host "Create backup: $LinkPath -> $backupPath"
+        Write-Host "Creating backup: $LinkPath -> $backupPath"
         Move-Item -Path $LinkPath -Destination $backupPath
     }
 
-    # Create symbolic link
-    Write-Host "Create symbolic link: $LinkPath -> $TargetPath"
+    Write-Host "Creating symbolic link: $LinkPath -> $TargetPath"
     sudo New-Item -ItemType SymbolicLink -Path $LinkPath -Target $TargetPath
 }
-
-# Use the directory where the script exists as the repository path
-$repoPath = $PSScriptRoot
-
-# User Profile Path
+#
+$repoPath = Split-Path $PSScriptRoot -Parent # Path to the root of the repository
 $userProfile = $Env:USERPROFILE
+$tempConfigPath = Join-Path $userProfile ".config_temp"
 
-# Setting up symbolic links
-Create-SymbolicLink -LinkPath "$userProfile\.config" -Target "$repoPath\.config"
-Create-SymbolicLink -LinkPath "$userProfile\.vifm" -Target "$repoPath\.config\.vifm"
-Create-SymbolicLink -LinkPath "$userProfile\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$repoPath\.config\windowsterminal\settings.json"
+# Create a temporary directory to hold the merged configuration
+New-Item -Path $tempConfigPath -ItemType Directory -Force
+
+# Copy common and windows specific configurations into the temporary directory
+Copy-Item -Path "$repoPath\common\.config\*" -Destination $tempConfigPath -Recurse -Force
+Copy-Item -Path "$repoPath\windows\.config\*" -Destination $tempConfigPath -Recurse -Force
+
+# Create symbolic link for the merged configuration
+Create-SymbolicLink -LinkPath "$userProfile\.config" -Target $tempConfigPath
+
+# Create additional symbolic links as needed
+Create-SymbolicLink -LinkPath "$userProfile\.vifm" -Target "$repoPath\common\.config\.vifm"
+Create-SymbolicLink -LinkPath "$userProfile\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$repoPath\windows\.config\windowsterminal\settings.json"
+Create-SymbolicLink -LinkPath "$userProfile\AppData\Local\nvim" -Target "$repoPath\common\.config\nvim"
 
 Write-Host "All symbolic links have been created."
+
+# Clean up temporary directory
+#Remove-Item -Path $tempConfigPath -Recurse -Force
 
