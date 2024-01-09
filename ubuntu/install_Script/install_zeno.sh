1465159e890cb9fc1941f5c5be0e489323110478
root309 <choco00101x@gmail.com>
2024-01-09 15:38:18 +0900

    cd install script zsh -> install_Script

---
+++ ubuntu/install_Script/install_zeno.sh
@@ -0,0 +1,6 @@
+zinit ice lucid depth"1" blockf
+zinit light yuki-yano/zeno.zsh
+
+zinit blockf light-mode as"program" from"gh-r" for \
+    atload'eval "$(zabrze init --bind-keys)"' \
+    Ryooooooga/zabrze
