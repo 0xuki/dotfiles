771a510fc9e9b587921892789d9d95de04699256
root309 <choco00101x@gmail.com>
2024-01-13 03:14:00 +0900

    Add theme

--- nix/modules/services.nix
+++ nix/modules/services.nix
@@ -4,11 +4,26 @@
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.xserver = {
+
+  services.xserver.displayManager.lightdm = {
     enable = true;
-    desktopManager.gnome.enable = true;
-    displayManager.gdm.enable = true;
+    greeter = {
+      mini = {
+        enable = true;
+        # LightDM-mini-greeterの設定
+        config = ''
+          [greeter]
+          background = /path/to/background/image
+          user-background = false
+          hide-user-list = true
+          default-user = yourusername
+          screen-keyboard = false
+          session = xmonad
+        '';
+      };
+    };
   };
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
