f0e9e474d507f1417cfcefee1b2e06d0ce1c411c
root309 <choco00101x@gmail.com>
2024-01-14 07:30:34 +0900

    fix: polybar

--- nix/home-manager/gui/polybar/default.nix
+++ nix/home-manager/gui/polybar/default.nix
@@ -1,53 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  polybarConfig = pkgs.writeText "polybar-config" ''
-    [bar/top]
-    font-0 = "Hack Regular:size=10;2"
-    font-1 = "Material Design Icons:size=10;2"
-    width = 100%
-    height = 32
-    background = #222222
-    foreground = #bbbbbb
-    modules-left = date cpu
-    modules-center = window
-    modules-right = memory battery
-
-    [module/date]
-    type = internal/date
-    interval = 5
-    date = "%Y-%m-%d"
-    time = "%H:%M:%S"
-    format-prefix = " "
-    format-prefix-foreground = "#ff0000"
-    format-foreground = "#ffffff"
-
-    [module/window]
-    type = internal/xwindow
-    format = "<label>"
-    format-foreground = "#ffffff"
-
-    [module/cpu]
-    type = internal/cpu
-    interval = 2
-    format = "CPU: <label>"
-    format-foreground = "#ffffff"
-
-    [module/memory]
-    type = internal/memory
-    format = "RAM: <label>"
-    format-foreground = "#ffffff"
-
-    [module/battery]
-    type = internal/battery
-    battery = 0
-    adapter = "ADP1"
-    format = "<label>"
-    format-foreground = "#ffffff"
-    format-discharging = "  <ramp-capacity>"
-    format-charging = "  <ramp-capacity>"
-  '';
-in
 {
   home.packages = with pkgs; [
     polybar
@@ -55,6 +7,9 @@ in
     material-design-icons # fonts with glyphs
   ];
 
-  home.file.".config/polybar/config.ini".source = polybarConfig;
+  home.file.".config/polybar/config.ini".source = ./config.ini;
+  home.file.".config/polybar/colors.ini".source = ./colors.ini;
+  home.file.".config/polybar/modules.ini".source = ./modules.ini;
+
 }
 
