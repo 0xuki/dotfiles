cab7509c0d628b46bb3e97a65d8032b5f420e91c
root309 <choco00101x@gmail.com>
2024-01-14 20:12:42 +0900

    update polybar

--- nix/home-manager/gui/polybar/default.nix
+++ nix/home-manager/gui/polybar/default.nix
@@ -9,15 +9,13 @@
   ];
 
   home.file.".config/polybar/config.ini".source = ./config.ini;
-  home.file.".config/polybar/colors.ini".source = ./colors.ini;
-  home.file.".config/polybar/system".source = ./system;
-  home.file.".config/polybar/modules/heatSensors".source = ./modules/heatSensors;
+  home.file.".config/polybar/colors.ini".source = ./colors.ini; 
   home.file.".config/polybar/modules/mediaModules".source = ./modules/mediaModules;
   home.file.".config/polybar/modules/modules".source = ./modules/modules;
   home.file.".config/polybar/modules/monitoringModules".source = ./modules/monitoringModules;
   home.file.".config/polybar/modules/separatorsModules".source = ./modules/separatorsModules;
-  home.file.".config/polybar/scripts/cava".source = ./scripts/cava;
-  home.file.".config/polybar/scripts/pipewire-microphone.sh".source = ./scripts/pipewire-microphone.sh; 
+  home.file.".config/polybar/scripts/cava".source = ./scripts/heatSensors;
+  home.file.".config/polybar/scripts/pipewire-microphone.sh".source = ./scripts/network.sh; 
 
 }
 
