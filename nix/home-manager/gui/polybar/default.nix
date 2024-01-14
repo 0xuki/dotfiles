16efcb17f8e5a895430d7e3c5c108234a28e23a4
root309 <choco00101x@gmail.com>
2024-01-14 20:04:52 +0900

    update polybar

--- nix/home-manager/gui/polybar/default.nix
+++ nix/home-manager/gui/polybar/default.nix
@@ -3,17 +3,21 @@
 {
   home.packages = with pkgs; [
     polybar
+    playerctl
     nerdfonts 
     material-design-icons # fonts with glyphs
   ];
 
   home.file.".config/polybar/config.ini".source = ./config.ini;
   home.file.".config/polybar/colors.ini".source = ./colors.ini;
-  home.file.".config/polybar/modules.ini".source = ./modules.ini;
-  home.file.".config/polybar/scripts/diskusage".source = ./scripts/diskusage;
-  home.file.".config/polybar/scripts/memory".source = ./scripts/memory;
-  home.file.".config/polybar/scripts/network".source = ./scripts/network;
-  home.file.".config/polybar/scripts/sblt".source = ./scripts/sblt;
+  home.file.".config/polybar/system".source = ./system;
+  home.file.".config/polybar/modules/heatSensors".source = ./modules/heatSensors;
+  home.file.".config/polybar/modules/mediaModules".source = ./modules/mediaModules;
+  home.file.".config/polybar/modules/modules".source = ./modules/modules;
+  home.file.".config/polybar/modules/monitoringModules".source = ./modules/monitoringModules;
+  home.file.".config/polybar/modules/separatorsModules".source = ./modules/separatorsModules;
+  home.file.".config/polybar/scripts/cava".source = ./scripts/cava;
+  home.file.".config/polybar/scripts/pipewire-microphone.sh".source = ./scripts/pipewire-microphone.sh; 
 
 }
 
