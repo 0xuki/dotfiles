55714d0d413f440d894f3c7a6baf560f6a0226ef
root309 <choco00101x@gmail.com>
2024-01-15 01:54:48 +0900

    change window manager xmonad -> hyprland, sway

--- nix/modules/desktop.nix
+++ nix/modules/desktop.nix
@@ -1,10 +1,110 @@
-{ config, pkgs, ... }:
+{ pkgs, user, ... }:
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.layout = "jp";
-  services.xserver.xkbVariant = "";
-  console.keyMap = "jp106";
-}
+  programs = {
+    dconf.enable = true;
+  };
 
+  programs.nm-applet = {
+    enable = true;
+    indicator = true;
+  };
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  security.pam.services.swaylock = { };
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    configPackages = [ pkgs.gnome.gnome-session ];
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-wlr ];
+  };
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons fcitx5-table-extra fcitx5-pinyin-moegirl fcitx5-pinyin-zhwiki ];
+  };
+  
+  i18n.defaultLocale = "ja_JP.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ja_JP.UTF-8";
+    LC_IDENTIFICATION = "ja_JP.UTF-8";
+    LC_MEASUREMENT = "ja_JP.UTF-8";
+    LC_MONETARY = "ja_JP.UTF-8";
+    LC_NAME = "ja_JP.UTF-8";
+    LC_NUMERIC = "ja_JP.UTF-8";
+    LC_PAPER = "ja_JP.UTF-8";
+    LC_TELEPHONE = "ja_JP.UTF-8";
+    LC_TIME = "ja_JP.UTF-8";
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      libnotify
+      wl-clipboard
+      wlr-randr
+      wayland
+      wayland-scanner
+      wayland-utils
+      egl-wayland
+      wayland-protocols
+      pkgs.xorg.xeyes
+      glfw-wayland
+      xwayland
+      pkgs.qt6.qtwayland
+      cinnamon.nemo
+      polkit_gnome
+      wev
+      wf-recorder
+      alsa-lib
+      alsa-utils
+      flac
+      pulsemixer
+      linux-firmware
+      sshpass
+      lxappearance
+      imagemagick
+      grim
+      slurp
+      linux-wifi-hotspot
+    ];
+    variables.NIXOS_OZONE_WL = "1";
+  };
+
+  services.xserver = {
+    xkbOptions = "caps:escape";
+  };
+  console.useXkbConfig = true;
+
+  programs = {
+    light.enable = true;
+  };
+  services = {
+    dbus.packages = [ pkgs.gcr ];
+    getty.autologinUser = "${user}";
+    gvfs.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+}
