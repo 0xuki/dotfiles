9a6da901f355573d989bbfb17b004aa37a3d4f3e
root309 <choco00101x@gmail.com>
2024-01-19 03:00:58 +0900

    update kitty

--- nix/home-manager/gui/tmux.nix
+++ nix/home-manager/gui/tmux.nix
@@ -4,18 +4,45 @@ let
   tmuxConfig = pkgs.writeTextFile {
     name = "tmux.conf";
     text = ''
-      set -g prefix C-a
-      setw -g mouse on
-      set -g history-limit 30000
-      set -g status-keys vi
-      set -g mode-keys vi
-      bind P paste-buffer
-      bind-key    -T copy-mode    C-w               send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
-      bind-key    -T copy-mode    MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "tmux-save-buffer - | xclip -selection primary -in -f | xclip -selection clipboard -in"
-      bind-key    -T copy-mode    M-w               send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
-      bind-key    -T copy-mode-vi Enter             send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
-      bind-key    -T copy-mode-vi v                 send-keys -X begin-selection
-      bind-key    -T copy-mode-vi y                 send-keys -X copy-pipe-and-cancel "xclip -selection primary -in -f | xclip -selection clipboard -in"
+      set -sg escape-time 50
+      # vim-like pane switching
+      bind -r k select-pane -U 
+      bind -r j select-pane -D 
+      bind -r h select-pane -L 
+      bind -r l select-pane -R 
+
+      # Make copy mode keystrokes vi-like.
+      set-window-option -g mode-keys vi
+
+      # Allow mouse wheel to scroll instead of history
+      set -g mouse on
+      set -g terminal-overrides 'xterm*:smcup@:rmcup@'
+
+      # When copying, also transfer to Windows clipboard (yum install -y xsel)
+      bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "xsel -bi"
+
+      # Change Prefix (Ctrl-b) to Ctrl-j
+      unbind-key C-b
+      set-option -g prefix C-j
+      bind-key C-S-j send-prefix
+
+      # Moving window
+      bind-key -n C-S-Left swap-window -t -1 \; previous-window
+      bind-key -n C-S-Right swap-window -t +1 \; next-window
+
+      # Resizing pane
+      bind -r C-k resize-pane -U 5
+      bind -r C-j resize-pane -D 5
+      bind -r C-h resize-pane -L 5
+      bind -r C-l resize-pane -R 5
+
+      # List of plugins
+      set -g @plugin 'tmux-plugins/tpm'
+      set -g @plugin 'tmux-plugins/tmux-sensible'
+      set -g @plugin "arcticicestudio/nord-tmux"
+
+      # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+      run '~/.tmux/plugins/tpm/tpm'
     '';
   };
 in {
@@ -24,6 +51,6 @@ in {
   ];
 
   # Tmuxの設定ファイルをユーザーのホームディレクトリに配置
-  xdg.configFile."tmux/tmux.conf".source = tmuxConfig;
+  home.file.".tmux.conf".source = tmuxConfig;
 }
 
