5bffca1860d9a5907248fb0bdf580a0ad5c192f8
root309 <choco00101x@gmail.com>
2024-01-28 22:21:37 +0900

    update startup pages link

--- nix/home-manager/programs/firefox/default.nix
+++ nix/home-manager/programs/firefox/default.nix
@@ -2,7 +2,7 @@
 let 
   bg = "/home/yuki/Pictures/wallpaper/wallpaper2.png";
   logo = "/home/yuki/Pictures/wallpaper/logo.png";
-  homepage = "/home/yuki/temp/homepage.html";
+  homepage = "https://root309.github.io/Bento/";
 in
 {
   home = {
@@ -23,7 +23,7 @@ in
     };
     profiles.default = {
       settings = {
-        "browser.startup.homepage" = "file://${homepage}";
+        "browser.startup.homepage" = "${homepage}";
       };
       userChrome = ''
               /*================== SIDEBAR ==================*/
