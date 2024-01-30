d1305832d10c882e3d5747a873fe2da45641e6b2
root309 <choco00101x@gmail.com>
2024-01-31 03:35:34 +0900

    w

--- nix/memo.sh
+++ nix/memo.sh
@@ -1,2 +1,3 @@
 nix run github:nix-community/home-manager -- switch --flake .#myHome -b backup --show-trace
 sudo nixos-rebuild switch --flake .#myNixOS
+nix build .#homeConfigurations.myHome.activationPackage
