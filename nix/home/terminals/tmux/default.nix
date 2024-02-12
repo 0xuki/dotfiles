6965241a98142e32a417d34653ec9fcdba11df61
root309 <choco00101x@gmail.com>
2024-02-12 23:49:37 +0900

    update tmux

--- nix/home/terminals/tmux/default.nix
+++ nix/home/terminals/tmux/default.nix
@@ -4,6 +4,10 @@ let
   tmuxConfig = pkgs.writeTextFile {
     name = ".tmux.conf";
     text = ''
+      # Terminal colors
+      set -g default-terminal "tmux-256color"
+      set -as terminal-features ",xterm-256color:RGB"
+
       set -sg escape-time 50
       # vim-like pane switching
       bind -r k select-pane -U 
@@ -37,25 +41,22 @@ let
       bind -r C-l resize-pane -R 5
 
       ## catppuccin config
-      set -g @catppuccin_window_left_separator "█"
+      set -g @catppuccin_directory_color "blue"
       set -g @catppuccin_window_right_separator "█ "
-      set -g @catppuccin_window_middle_separator " █"
       set -g @catppuccin_window_number_position "right"
+      set -g @catppuccin_window_middle_separator " | "
+      set -g @catppuccin_window_left_separator "█"
 
-      set -g @catppuccin_window_default_fill "number"
-      set -g @catppuccin_window_default_text "#W"
+      set -g @catppuccin_window_default_fill "all"
 
-      set -g @catppuccin_window_current_fill "number"
-      set -g @catppuccin_window_current_text "#W"
+      set -g @catppuccin_window_current_fill "all"
 
-      set -g @catppuccin_status_modules_right "directory user host session"
-      set -g @catppuccin_status_left_separator  " █"
+      set -g @catppuccin_status_modules_right "application session user host date_time"
+      set -g @catppuccin_status_left_separator "█"
       set -g @catppuccin_status_right_separator "█"
-      set -g @catppuccin_status_right_separator_inverse "no"
-      set -g @catppuccin_status_fill "icon"
-      set -g @catppuccin_status_connect_separator "no"
 
-      set -g @catppuccin_directory_text "#{pane_current_path}"
+      set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M:%S"      
+
       # List of plugins
       set -g @plugin 'tmux-plugins/tpm'
       set -g @plugin 'tmux-plugins/tmux-sensible'
