599adee7b483e2f1b46f9b315712bce2533084e9
root309 <choco00101x@gmail.com>
2024-02-16 15:24:00 +0900

    update powermanagement

--- nix/modules/desktop.nix
+++ nix/modules/desktop.nix
@@ -137,6 +137,24 @@ in
         TimeoutStopSec = 10;
       };
     };
+    targets = {
+      sleep = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      suspend = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      hibernate = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      "hybrid-sleep" = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+    };
   };
 
   hardware = {
