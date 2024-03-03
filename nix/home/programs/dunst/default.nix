a574caeaba63cec424bd14825e579acaa8933a7c
root309 <choco00101x@gmail.com>
2024-03-03 23:00:02 +0900

    a

--- nix/home/programs/dunst/default.nix
+++ nix/home/programs/dunst/default.nix
@@ -1,10 +1,21 @@
 { pkgs, ... }:
 {
-  home.packages = [ 
-    pkgs.dunst 
-  ];
+  home.packages = [ pkgs.dunst ];
+
   home.file = {
     ".config/dunst/dunst.conf".source = ./dunst.conf;
     ".config/dunst/dunstrc".source = ./dunstrc;
+  };
+
+  home.activationScripts.icons = {
+    text = ''
+      $DRY_RUN_CMD mkdir -p ${pkgs.stdenv.mkDerivation {
+        name = "home-config-dunst";
+        phases = [ "installPhase" "fixupPhase" ];
+        installPhase = "ln -sfn ${./icons} $out/.config/dunst/icons";
+        buildInputs = [ pkgs.makeWrapper ];
+      }}/home/yuki/.config/dunst
+    '';
+    deps = [ "dunst" ];
   };
 }
