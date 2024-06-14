c03f802241c7f193a660fedb5608963b81482b5c
0xuki <choco00101x@gmail.com>
2024-06-14 21:31:40 +0900

    ✨ (noice.lua) change cmdline view position

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -6,4 +6,22 @@ return {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
     },
+    config = function()
+        require("noice").setup({
+            cmdline = {
+            view = "cmdline_popup",
+            opts = {
+              position = {
+                row = 10,
+                col = "50%"  -- 中央に配置
+              },
+              size = {
+                width = "50%",
+                height = 1
+              },
+              relative = "editor"
+            }
+            },
+        })
+    end,
 }
